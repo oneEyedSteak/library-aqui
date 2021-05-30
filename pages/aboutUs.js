366ccead0a08eeace953768269603791ffa5b640
crrmacarse <crrmacarse@gmail.com>
2021-05-31 01:59:55 +0800

    eee

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -11,9 +11,9 @@ const AboutUs = () => {
         </Head>
 
         <div className='from-blue-900 to-yellow-600 bg-gradient-to-br min-h-screen mx-auto'>  
-        <h1>
-            ABOUT US
-        </h1>
+        <h2>
+            ABOUT 
+        </h2>
         </div>
        </> 
         );
