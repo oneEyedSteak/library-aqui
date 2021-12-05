5758d2d501ca312e66bb45e11b5f5f717e859232
OneEyedSteak <stalinsteam06@gmail.com>
2021-12-05 12:33:49 +0800

    printFixed

--- pages/see-all-books-to-print.js
+++ pages/see-all-books-to-print.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import mysql from '../providers/mysql';
 import validateSession from '../lib/session';
+import { useMemo } from 'react';
+import Link from 'next/link';
+import ReactTable from '../components/table';
 
 import BooksToPrint from '../components/BooksToPrint';
 
@@ -20,6 +23,99 @@ export const getServerSideProps = async (context) => {
 };
 export default function seeAllBooksCustodian({ booksToPrint }) {
   console.log(booksToPrint);
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Request ID',
+        accessor: 'requestID', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Request Date',
+        accessor: 'date',
+        Cell: ({ row: { values } }) => (
+          <div>
+            {new Date(values.date).toDateString()}
+          </div>
+        ),
+      },
+      {
+        Header: 'Athor Name',
+        accessor: 'authorName',
+      },
+      {
+        Header: 'Title',
+        accessor: 'title', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Publish Date',
+        accessor: 'pubdate', // accessor is the "key" in the data
+        Cell: ({ row: { values } }) => (
+          <div>
+            {new Date(values.pubdate).toDateString()}
+          </div>
+        ),
+      },
+      {
+        Header: 'User ID',
+        accessor: 'userID', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Requested By',
+        accessor: 'requestee', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Department',
+        accessor: 'selectDepartment', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Is Rush?',
+        accessor: 'rushornrush',
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+
+      {
+        Header: 'Approved By Acquisition',
+        accessor: 'approvalAcqui', // accessor is the "key" in the data
+        Cell: ({ row: { values } }) => (
+          <div>
+            {values.approvalAcqui ? 'Yes' : 'No'}
+          </div>
+        ),
+      },
+      {
+        Header: 'Approved By Director',
+        accessor: 'approvalDirector', // accessor is the "key" in the data
+        Cell: ({ row: { values } }) => (
+          <div>
+            {values.approvalDirector ? 'Yes' : 'No'}
+          </div>
+        ),
+      },
+
+
+      {
+        Header: () => 'Action',
+        accessor: 'action',
+        Cell: ({ row: { values } }) => (
+          <Link href={`/books-to-print/${values.requestID}`}>
+
+            <button
+              type="button"
+              className="mx-auto mt-3  text-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md
+                                     text-white bg-indigo-600 hover:bg-indigo-700
+                                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+            Print 
+            </button>
+          </Link>
+        ),
+      },
+    ],
+    [],
+  );
 
   return (
     <>
@@ -28,27 +124,25 @@ export default function seeAllBooksCustodian({ booksToPrint }) {
         <meta name="keywords" content="someting" />
         <link rel="icon" href="/icon.ico" />
       </Head>
-      <section className="max-w-screen from-blue-900 to-yellow-600 bg-gradient-to-br  min-h-screen mx-auto ">
+      <section className="max-w-screen bg-base min-h-screen mx-auto ">
 
-        <div className=" mx-auto  min-w-full  sm:px-6 lg:px-8">
-          <img src="/1234.jpg" className="  min-w-full h-10/12 rounded-xl mx-autos" alt="1234" />
-        </div>
-        <h2 className=" bg-gray-500 text-gray-100 text-center w-3/4 mx-auto p-4 mt-5 rounded">
-          All Requested Book
-        </h2>
-        <div className="p-28 grid grid-cols-3 gap-1
-        sm:grid-cols-5 md:grid-cols-5 lg:grid-cols-5
-         xl:grid-cols-4"
-        >
-          {
+<form className=" p-14 bg-white rounded-md my-16 w- mx-auto h-auto w-auto shadow-lg ">
 
-                        booksToPrint && booksToPrint.map((reqbook) => (
-                          <BooksToPrint booksToPrint={reqbook} />
-                        ))
+  <div className="flex-shrink-0 flex content-around items-center">
 
-                    }
-        </div>
-      </section>
+    <img className="hidden lg:block h-14 w-auto  mr-3" src="/cpulogo.png" alt="okay" />
+    <img className="block lg:hidden h-14 w-auto  mr-3" src="/cpulogo.png" alt="cpu logo" />
+    <h1 className="text-xl  text-gray-600 ">Approved Books by Finance</h1>
+
+  </div>
+  <div className="text-xs shadow-md w-full mt-10 ">
+    <span className="block  text-xs  text-gray-500 "> All Books</span>
+
+    <ReactTable data={booksToPrint} columns={columns} />
+  </div>
+</form>
+
+</section>
     </>
   );
 }
